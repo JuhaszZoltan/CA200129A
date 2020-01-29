24c6fbcba2c95ca35c9bd865b60f1fb78a08ebb8
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2020-01-29 19:22:17 +0100

    Add project files.

---
+++ CA200129A/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200129A
+{
+    struct Jatek
+    {
+        public int ev;
+        public string cim;
+        public string mufaj;
+        public string kiado;
+        public string[] platformok;
+    }
+    class Program
+    {
+        static List<Jatek> jatekok;
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            F02();
+            F03();
+            F04();
+            F05();
+            F06();
+            F07();
+            F08();
+            F09();
+            F10();
+            Console.ReadKey();
+        }
+
+        private static void F10()
+        {
+            Console.Write("10. feladat:\n\t");
+            int eao = 0;
+            int eapc = 0;
+            foreach (var j in jatekok)
+            {
+                if(j.kiado == "Electronic Arts")
+                {
+                    eao++;
+                    if(j.platformok.Contains("PC"))
+                    {
+                        eapc++;
+                    }
+                }
+            }
+            Console.WriteLine(
+                "Az EA díjazott játékainak {0:0.00}%-a jelent meg PC-re",
+                eapc * 100 / (float)eao);
+        }
+        private static void F09()
+        {
+            Console.Write(" 9. feladat:\n\t");
+            Console.Write("Írja be egy platform nevét: ");
+            var plat = Console.ReadLine();
+
+            var pj = new List<string>();
+            foreach (var j in jatekok)
+            {
+                if (j.platformok.Contains(plat)) pj.Add(j.cim);
+            }
+
+            if(pj.Count != 0)
+            {
+                for (int j = 0; j < pj.Count; j++)
+                {
+                    int x = rnd.Next(pj.Count);
+                    int y = rnd.Next(pj.Count);
+                    var s = pj[x];
+                    pj[x] = pj[y];
+                    pj[y] = s;
+                }
+
+                Console.WriteLine($"\t{plat} platformra pldául ezek a játékok jelentek meg:");
+                int i = 0;
+                while (i < 10 && i < pj.Count)
+                {
+                    Console.WriteLine("\t\t{0, 2}.: {1}", i + 1, pj[i]);
+                    i++;
+                }
+            }
+            else Console.WriteLine("\tIlyen platform nincs a file-ban");
+        }
+        private static void F08()
+        {
+            var mufajok = new List<string>();
+            foreach (var j in jatekok)
+            {
+                if (!mufajok.Contains(j.mufaj)) mufajok.Add(j.mufaj);
+            }
+            mufajok.Sort();
+            var sw = new StreamWriter(@"..\..\res\genres.txt", false, Encoding.UTF8);
+            foreach (var m in mufajok) sw.WriteLine(m);
+            sw.Close();
+        }
+        private static void F07()
+        {
+            //dictionary... soon
+        }
+        private static void F06()
+        {
+            Console.Write(" 6. feladat:\n\t");
+            var lng = new Jatek() { ev = int.MinValue, };
+            foreach (var j in jatekok)
+            {
+                if(j.platformok.Contains("NES") && j.ev > lng.ev)
+                    lng = j;
+            }
+            Console.WriteLine(
+                $"A legkésőbbi NES megjelenés a listába: {lng.cim} ({lng.ev})");
+        }
+        private static void F05()
+        {
+            //dictionary... soon
+        }
+        private static void F04()
+        {
+            Console.Write(" 4. feladat:\n\t");
+
+            int db = 0;
+            foreach (var j in jatekok)
+            {
+                if (j.platformok.Contains("PC")) db++;
+            }
+            Console.WriteLine($"{db} db játk jelent meg PC platformra is!");
+
+        }
+        private static void F03()
+        {
+            Console.Write(" 3. feladat:\n\t");
+            Console.WriteLine($"Összesen {jatekok.Count} játék szerepel a listában");
+        }
+        private static void F02()
+        {
+            jatekok = new List<Jatek>();
+            var sr = new StreamReader(@"..\..\res\games.txt", Encoding.UTF8);
+            while (!sr.EndOfStream)
+            {
+                var s = sr.ReadLine().Split(';');
+                var j = new Jatek()
+                {
+                    ev = int.Parse(s[0]),
+                    cim = s[1],
+                    mufaj = s[2],
+                    kiado = s[3],
+                    platformok = s[4].Split(','),
+                };
+                jatekok.Add(j);
+            }
+            sr.Close();
+        }
+    }
+}
